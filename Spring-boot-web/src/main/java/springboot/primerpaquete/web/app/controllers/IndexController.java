08b0ae1756efb80ea4545be1c34a7f2644c8e4fa
lukenavarro <44287208+lukenavarro@users.noreply.github.com>
2023-02-27 23:56:46 +0100

    Utilizacion del Model, Model And View, Model Map y Map

--- Spring-boot-web/src/main/java/springboot/primerpaquete/web/app/controllers/IndexController.java
+++ Spring-boot-web/src/main/java/springboot/primerpaquete/web/app/controllers/IndexController.java
@@ -3,11 +3,16 @@ package springboot.primerpaquete.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 //Utilizamos el @Controller para establecer el controlador
 @Controller
 public class IndexController {
+
     /**
      * Mapeamos el meotodo con @RequestMapping(ruta, method = "Metodo"), @GetMapping("ruta") o PostMapping({"ruta1","ruta2"}) como si fuese un array.
      *
@@ -20,5 +25,30 @@ public class IndexController {
         // añadimos al model(K,V) y lo enviamos al HMTL index.
         model.addAttribute("titulo","hola Springboot con Model!");
         return "index";
+    }
+    //Uso con Model Map Hereda del Mapa de Java.
+    @GetMapping("/ModelMap")
+    public String index(ModelMap model){
+        model.addAttribute("titulo","Hola SpringBoot con ModelMap!");
+        return "index";
+    }
+    // Uso con el Map
+    @GetMapping("/map")
+    public String index(Map<String,Object> map){
+        map.put("titulo","Hola Springboot con Map!");
+        return "index";
+    }
+    /*
+        Otra forma de hacer lo anterior es con el model and view
+        Para ello devolveremos un objeto ModelAndView
+        En este añadiremos el objeto K V
+        Añadimos el SetViewName con el index.html
+        Lo devolvemos para sacarlo por pantalla.
+     */
+    @GetMapping("/ModelAndView")
+    public ModelAndView index(ModelAndView mv){
+        mv.addObject("titulo","Hola Springboot con Model And view!");
+        mv.setViewName("index");
+        return mv;
     }
 }
