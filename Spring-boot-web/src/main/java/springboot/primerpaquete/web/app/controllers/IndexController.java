3f264cb1a05bb30993b9435f81288c49aa0dacee
lukenavarro <44287208+lukenavarro@users.noreply.github.com>
2023-02-27 22:50:51 +0100

    Comentario y subida del metodo mediante Thymeleaf

--- Spring-boot-web/src/main/java/springboot/primerpaquete/web/app/controllers/IndexController.java
+++ Spring-boot-web/src/main/java/springboot/primerpaquete/web/app/controllers/IndexController.java
@@ -1,14 +1,24 @@
 package springboot.primerpaquete.web.app.controllers;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-
-@RestController
+//Utilizamos el @Controller para establecer el controlador
+@Controller
 public class IndexController {
+    /**
+     * Mapeamos el meotodo con @RequestMapping(ruta, method = "Metodo"), @GetMapping("ruta") o PostMapping({"ruta1","ruta2"}) como si fuese un array.
+     *
+     * @param model
+     * @return
+     */
     @GetMapping({"/index","/","/home"})
-    public String index(){
+    public String index(Model model){
+
+        // añadimos al model(K,V) y lo enviamos al HMTL index.
+        model.addAttribute("titulo","hola Springboot!");
         return "index";
     }
 }
